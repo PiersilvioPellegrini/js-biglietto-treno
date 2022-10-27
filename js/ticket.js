2910e88058d55d1455bdb272670a11844c7a90e1
PiersilvioPellegrini <pier.pellegrini.23@gmail.com>
2022-10-27 17:30:26 +0200

    completed

--- js/ticket.js
+++ js/ticket.js
@@ -8,11 +8,27 @@ const prezzoBiglietto = numeroKm * 0.21;
 let sconto = 0;
 if (Age < 18) {
     sconto = prezzoBiglietto * 0.8;
+    console.log(sconto.toFixed(2));
 }
 else if (Age > 65) {
     sconto = prezzoBiglietto * 0.6;
+    console.log(sconto.toFixed(2));
 }
 
 // Calcolo prezzo finale
 const prezzoFinale = prezzoBiglietto - sconto;
 console.log(prezzoFinale.toFixed(2));
+
+const price = document.getElementById("ticketPrice");
+
+price.innerHTML = prezzoFinale.toFixed(2);
+
+if(sconto>0){
+const discount = document.getElementById("discount");
+discount.innerHTML = "Hai diritto ad uno sconto di: " + sconto.toFixed(2) + "€";
+}
+
+else{
+const discount = document.getElementById("discount");
+discount.innerHTML = "Non hai diritto ad uno sconto";
+}
